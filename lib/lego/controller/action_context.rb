8ab21b89bde94fa78964924649e58bcb740ed96b
Mathias Stjernström <mathias@globalinn.com>
2010-01-11 00:05:55 +0100

    Changed route[:action_block] data to be appended to @response[:body]

--- lib/lego/controller/action_context.rb
+++ lib/lego/controller/action_context.rb
@@ -21,14 +21,19 @@ private
   # setup_instance_vars_from_route extracts variables found in route[:instance_vars] thats setup by the route matchers
   # and converts them to instance variables which makes them availible to the ActionContext.
   #
+
   def setup_instance_vars_from_route(route, env)
     route[:instance_vars].each_key do |var|
       instance_variable_set("@#{var}", route[:instance_vars][var])
     end if route[:instance_vars]
   end
 
+  #
+  # evaluate_action executes route[:action_block] and appends the output to @response[:body]
+  #
+
   def evaluate_action(route)
-    @response[:body] = instance_eval(&route[:action_block]) if route[:action_block]
+    @response[:body] << instance_eval(&route[:action_block]) if route[:action_block]
   end
 
   def setup_defaults
