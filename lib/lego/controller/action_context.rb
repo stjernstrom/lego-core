46af8457621bf69fc6262950792af68e00e82215
Mathias Stjernström <mathias@globalinn.com>
2010-01-11 00:02:54 +0100

    Added extraction and conversion of route[:instance_vars] to instance vars in ActionContext

--- lib/lego/controller/action_context.rb
+++ lib/lego/controller/action_context.rb
@@ -10,11 +10,22 @@ class Lego::Controller::ActionContext
   end
 
   def run(route, env)
+    setup_instance_vars_from_route(route, env)
     evaluate_action(route)
     [@response[:code], @response[:headers], @response[:body]]
   end
 
 private
+
+  #
+  # setup_instance_vars_from_route extracts variables found in route[:instance_vars] thats setup by the route matchers
+  # and converts them to instance variables which makes them availible to the ActionContext.
+  #
+  def setup_instance_vars_from_route(route, env)
+    route[:instance_vars].each_key do |var|
+      instance_variable_set("@#{var}", route[:instance_vars][var])
+    end if route[:instance_vars]
+  end
 
   def evaluate_action(route)
     @response[:body] = instance_eval(&route[:action_block]) if route[:action_block]
