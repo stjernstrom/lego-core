b9c0bf27ec590d9da003c65424bccadc6f5d8aff
Patrik Hedman <patrik@moresale.se>
2010-01-11 21:49:53 +0100

    removed environment specific specs and added proper indentation

--- spec/lego/controller/config_spec.rb
+++ spec/lego/controller/config_spec.rb
@@ -4,171 +4,58 @@ describe Lego::Controller::Config do
 
   context ".set options" do
 
-   context "on Lego::Controller" do
-
-     before do
-       create_new_app "App1", Lego::Controller
-       create_new_app "App2", Lego::Controller
-       Lego::Controller.set :foo => "bar"
-     end
-
-     it "should be accessible to Lego::Controller" do
-       Lego::Controller.current_config.options(:foo).should eql("bar")
-     end
-
-     it "should be accessible to App1" do
-       App1.current_config.options(:foo).should eql("bar")
-     end
-
-     it "should be accessible to App2" do
-       App2.current_config.options(:foo).should eql("bar")
-     end
-
-     after do
-       rm_const "App1", "App2"
-     end
-   end
-
-   context "on Lego::Controller subclasses" do
-
-     before do
-       create_new_app "App1", Lego::Controller
-       create_new_app "App2", Lego::Controller
-       App1.set :app1 => "App1"
-     end
-
-     it "should be available to App1" do
-       App1.current_config.options(:app1).should eql("App1")
-     end
-
-     it "should NOT be available to App2" do
-       App2.current_config.options(:app1).should eql(nil)
-     end
-
-     it "should NOT be available to Lego::Controller" do
-       App2.current_config.options(:app1).should eql(nil)
-     end
-
-     after do
-       rm_const "App1", "App2"
-     end
-   end
-  end 
-
-  context ".environment [env], &block" do
-    
     context "on Lego::Controller" do
 
-      context "without environment" do
-        before do
-          create_new_app "App1", Lego::Controller
-          create_new_app "App2", Lego::Controller
-          Lego::Controller.environment do
-            set :bar => "baz"
-          end
-        end
-
-        it "should be available to Lego::Controller" do
-          Lego::Controller.current_config.options(:bar).should eql("baz")
-        end
-
-        it "should be available to App1" do
-          App1.current_config.options(:bar).should eql("baz")
-        end
-
-        it "should be available to App1" do
-          App2.current_config.options(:bar).should eql("baz")
-        end
-
-        after do
-          rm_const "App1", "App2"
-        end
+      before do
+        create_new_app "App1", Lego::Controller
+        create_new_app "App2", Lego::Controller
+        Lego::Controller.set :foo => "bar"
       end
 
-      context "with environment" do
-        
-        before do
-          create_new_app "App1", Lego::Controller
-          Lego::Controller.environment(:development) { set :current_env => "development" }
-          Lego::Controller.environment(:production)  { set :current_env => "production"  }
-        end
 
-        it "should default to development" do
-          Lego::Controller.current_config.options(:current_env).should eql("development")
-        end
 
-        it 'should use ENV["RACK_ENV"] if available' do
-          ENV['RACK_ENV'] = 'production'
-          Lego::Controller.current_config.options(:current_env).should eql("production")
-          ENV['RACK_ENV'] = nil
-        end
+      it "should be accessible to Lego::Controller" do
+         Lego::Controller.current_config.options(:foo).should eql("bar")
+      end
 
-        it "should be available to subclasses" do
-          App1.current_config.options(:current_env).should eql("development")
-        end
+      it "should be accessible to App1" do
+         App1.current_config.options(:foo).should eql("bar")
+      end
 
-        after do
-          rm_const "App1"
-        end
+      it "should be accessible to App2" do
+         App2.current_config.options(:foo).should eql("bar")
+      end
+
+      after do
+         rm_const "App1", "App2"
       end
     end
 
-    context "on App1" do
+    context "on Lego::Controller subclasses" do
 
-      context "without environment" do
-
-        before do
-          create_new_app "App1", Lego::Controller
-          create_new_app "App2", Lego::Controller
-          App1.environment { set :my_var => "foo" }
-        end
-
-        it "should be available to App1" do
-          App1.current_config.options(:my_var).should eql("foo")
-        end
-
-        it "should not be available to App2" do
-          App2.current_config.options(:my_var).should eql(nil)
-        end
-
-        after do
-          rm_const "App1", "App2"
-        end
+      before do
+        create_new_app "App1", Lego::Controller
+        create_new_app "App2", Lego::Controller
+        App1.set :app1 => "App1"
       end
 
-      context "with environment" do
+      it "should be available to App1" do
+        App1.current_config.options(:app1).should eql("App1")
+      end
 
-        before do
-          create_new_app "App1", Lego::Controller
-          create_new_app "App2", Lego::Controller
-          App1.environment(:development) { set :my_env => "development" }
-          App1.environment(:production)  { set :my_env => "production"  }
-        end
+      it "should NOT be available to App2" do
+        App2.current_config.options(:app1).should eql(nil)
+      end
 
-        it "should default to development" do
-          App1.current_config.options(:my_env).should eql("development")
-        end
+      it "should NOT be available to Lego::Controller" do
+        App2.current_config.options(:app1).should eql(nil)
+      end
 
-        it "should use ENV['RACK_ENV'] if available" do
-          ENV['RACK_ENV'] = 'production'
-          App1.current_config.options(:my_env).should eql("production")
-          ENV['RACK_ENV'] = nil
-        end
-
-        it "should not be available to App2" do
-          App2.current_config.options(:my_env).should eql(nil)
-        end
-
-        it "should not be available to Lego::Controller" do
-          Lego::Controller.current_config.options(:my_env).should eql(nil)
-        end
-
-        after do
-          rm_const "App1", "App2"
-        end
+      after do
+        rm_const "App1", "App2"
       end
     end
-  end
+  end 
 
   context ".options key" do
 
