277f322bd69799120ddcca2eff159aa7a1c6ec78
Patrik Hedman <patrik@moresale.se>
2010-02-03 22:36:50 +0100

    improved object-ext spec

--- spec/lego/core-ext/object_ext_spec.rb
+++ spec/lego/core-ext/object_ext_spec.rb
@@ -4,33 +4,41 @@ class Dummy
   end
 end
 
-describe "Instance exec" do
-  
-  let(:dummy) { Dummy.new }
+describe Object do
+  context ".instance_exec" do
+    
+    let(:dummy) { Dummy.new }
 
-  it "with arguments" do
-    # Create a block that returns the value of an argument and a value
-    # of a method call to +self+.  
-    block = lambda { |a| [a, f] }
+    context "with argumets" do
 
-    [:arg_value, :dummy_value].should eql(dummy.instance_exec(:arg_value, &block))
-  end
+      it "should return the value of an argument and a method call to +self+" do
+        block = lambda { |a| [a, f] }
 
-  it "with frozen obj" do
-    block = lambda { |a| [a, f] }
-
-    dummy.freeze
-    [:arg_value, :dummy_value].should eql(dummy.instance_exec(:arg_value, &block))
-  end
-
-  it "with nested instance_execs" do
-    i = 0
-    block = lambda do |arg|
-      [arg] + instance_exec(1){|a| [f, a] }
+        [:arg_value, :dummy_value].should eql(dummy.instance_exec(:arg_value, &block))
+      end
     end
 
-    # the following assertion expanded by the xmp filter automagically from:
-    # obj.instance_exec(:arg_value, &block) #=>
-    [:arg_value, :dummy_value, 1].should eql(dummy.instance_exec(:arg_value, &block))
+    context "with frozen object" do
+
+      it "should return the value of an argument and a method call to +self+" do
+        block = lambda { |a| [a, f] }
+
+        dummy.freeze
+        [:arg_value, :dummy_value].should eql(dummy.instance_exec(:arg_value, &block))
+      end
+    end
+
+    context "with nested instance_execs" do
+      it "should return the value of an argument and a method call to +self+" do
+        i = 0
+        block = lambda do |arg|
+          [arg] + instance_exec(1){|a| [f, a] }
+        end
+
+        # the following assertion expanded by the xmp filter automagically from:
+        # obj.instance_exec(:arg_value, &block) #=>
+        [:arg_value, :dummy_value, 1].should eql(dummy.instance_exec(:arg_value, &block))
+      end
+    end
   end
 end
